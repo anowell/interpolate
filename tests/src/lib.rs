3769b49ded94f7839343787a59e68b12fa4164cf
Anthony Nowell <anowell@gmail.com>
2023-02-22 02:43:49 -0800

    Experimenting with prefixing string literals

--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -1,99 +1,33 @@
-#![feature(proc_macro_hygiene)]
-#![feature(non_ascii_idents)]
-
 extern crate interpolate;
 
 #[cfg(test)]
 mod tests {
-    use interpolate::s;
+    use interpolate::fstring;
+
+    fn f() -> &'static str {
+        "hi"
+    }
 
     #[test]
-    fn wrapped() {
+    #[fstring]
+    fn no_args() {
+        assert_eq!("Hello", f"Hello");
+    }
+
+    #[test]
+    #[fstring]
+    fn interpolation() {
         let name  = "Elsa";
-        assert_eq!("Hello, Elsa.", s!("Hello, {name}."));
-        assert_eq!("Hello, Elsa.", s!("Hello, { name }."));
+        assert_eq!("Hello, Elsa.", f"Hello, {name}.");
     }
 
     #[test]
-    fn prefix() {
-        let name  = "Mulan";
-        assert_eq!("Mulan, the brave.", s!("{name}, the brave."));
-        assert_eq!("Mulan, the brave.", s!("{ name }, the brave."));
+    #[fstring]
+    fn ident_collision() {
+        let msg  = f();
+        let f = "hello";
+        assert_eq!("hi", msg);
+        assert_eq!("hello", f);
     }
 
-    #[test]
-    fn suffix() {
-        let name  = "Jafar";
-        assert_eq!("Vile Jafar", s!("Vile {name}"));
-        assert_eq!("Vile Jafar", s!("Vile { name }"));
-    }
-
-    #[test]
-    fn pre_and_suffix() {
-        let name  = "Lilo";
-        let name2 = "Stitch";
-        assert_eq!("Lilo and Stitch", s!("{name} and {name2}"));
-    }
-
-    #[test]
-    fn single_expression_only() {
-        let name  = "Prince Charming";
-        assert_eq!("Prince Charming", s!("{name}"));
-    }
-
-    #[test]
-    fn escapes() {
-        assert_eq!("{ input", s!("{{ input"));
-        assert_eq!("} input", s!("}} input"));
-        let val = "foo";
-        assert_eq!("{ foo }", s!("{{ {val} }}"));
-        // Please don't actually do this...
-        assert_eq!("f", s!("{val.chars().filter(|c| {{ *c=='f' }}).collect::<String>()}"));
-    }
-
-
-    #[test]
-    fn connected_expressions() {
-        let first_name  = "Mickey";
-        let last_name = "Mouse";
-        assert_eq!("MickeyMouse", s!("{first_name}{last_name}"));
-        assert_eq!("MickeyMouse", s!("{ first_name }{ last_name }"));
-    }
-
-    #[test]
-    fn no_expression() {
-        assert_eq!("Doc", s!("Doc"));
-    }
-
-    #[test]
-    fn reuse() {
-        let name = "Aladdin";
-        assert_eq!(
-            "Aladdin is the star of Aladdin.",
-            s!("{name} is the star of {name}.")
-        );
-    }
-
-    #[test]
-    fn xid_start_continue() {
-        let _full_name_  = "Judy Hopps";
-        assert_eq!("Judy Hopps!", s!("{_full_name_}!"));
-
-        #[allow(non_snake_case)]
-        let Ö𞹼𝒜𝟋ↈ  = "Olaf";
-        assert_eq!("~Olaf~", s!("~{Ö𞹼𝒜𝟋ↈ}~"));
-    }
-
-    #[test]
-    fn expressions() {
-        let ducks = vec!["Huey", "Dewey", "Louie"];
-        assert_eq!("Dewey", s!("{ducks[1]}"));
-        assert_eq!("HueyDeweyLouie", s!("{ducks.concat()}"));
-
-        // Please, don't actually do this
-        assert_eq!(
-            "hueydeweylouie",
-            s!("{ducks.iter().map(|s|s.to_lowercase()).collect::<Vec<_>>().concat()}")
-        );
-    }
 }
